e0a4d5911612c92f9bae7184e38c3f426e28a9f7
paranora <yangqun@centaline.com.cn>
2019-07-03 11:08:30 +0800

    'u'

--- sentinel-dashboard-apollo/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/apollo/ApolloConfigUtil.java
+++ sentinel-dashboard-apollo/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/apollo/ApolloConfigUtil.java
@@ -23,7 +23,7 @@ public final class ApolloConfigUtil {
 
     public static final String FLOW_DATA_ID_POSTFIX = "-flow-rules";
     public static final String DEGRADE_DATA_ID_POSTFIX = "-degrade-rules";
-    public static final String PARAM_FLOW_DATA_ID_POSTFIX = "-paramflow-rules";
+    public static final String PARAM_FLOW_DATA_ID_POSTFIX = "-param-flow-rules";
     public static final String SYSTEM_DATA_ID_POSTFIX = "-system-rules";
     public static final String AUTH_DATA_ID_POSTFIX = "-auth-rules";
 
