88574563b3e1b0a42715773caee39d3eedd0ba98
paranora <yangqun@centaline.com.cn>
2019-08-26 15:03:23 +0800

    'update'

--- sentinel-dashboard-apollo/src/main/webapp/resources/app/scripts/directives/header/header.js
+++ sentinel-dashboard-apollo/src/main/webapp/resources/app/scripts/directives/header/header.js
@@ -5,26 +5,43 @@
  * # adminPosHeader
  */
 angular.module('sentinelDashboardApp')
-  .directive('header', ['AuthService', function () {
-    return {
-      templateUrl: 'app/scripts/directives/header/header.html',
-      restrict: 'E',
-      replace: true,
-      controller: function ($scope, $state, $window, AuthService) {
-        if (!$window.localStorage.getItem('session_sentinel_admin')) {
-          $state.go('login');
-        }
+    .directive('header', ['AuthService', function () {
+      return {
+        templateUrl: 'app/scripts/directives/header/header.html',
+        restrict: 'E',
+        replace: true,
+        controller: function ($scope, $state, $window, AuthService) {
+          if (!$window.localStorage.getItem("session_sentinel_admin")) {
+            AuthService.check().success(function (data) {
+              if (data.code == 0) {
+                $window.localStorage.setItem('session_sentinel_admin', JSON.stringify(data.data));
+                handleLogout($scope, data.data.id)
+              } else {
+                $state.go('login');
+              }
+            });
+          } else {
+            handleLogout($scope, JSON.parse($window.localStorage.getItem("session_sentinel_admin")).id)
+          }
 
-        $scope.logout = function () {
-          AuthService.logout().success(function (data) {
-            if (data.code == 0) {
-              $window.localStorage.removeItem("session_sentinel_admin");
-              $state.go('login');
+          function handleLogout($scope, id) {
+            if (id == 'FAKE_EMP_ID') {
+              $scope.showLogout = false;
             } else {
-              alert('logout error');
+              $scope.showLogout = true;
             }
-          });
+          }
+
+          $scope.logout = function () {
+            AuthService.logout().success(function (data) {
+              if (data.code == 0) {
+                $window.localStorage.removeItem("session_sentinel_admin");
+                $state.go('login');
+              } else {
+                alert('logout error');
+              }
+            });
+          }
         }
       }
-    }
-  }]);
+    }]);
