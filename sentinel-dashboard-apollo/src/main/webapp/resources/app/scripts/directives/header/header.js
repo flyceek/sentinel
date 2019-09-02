be78ed2b3d1971911cb5afe1be077af565e587f7
paranora <yangqun@centaline.com.cn>
2019-09-02 10:39:09 +0800

    'update'

--- sentinel-dashboard-apollo/src/main/webapp/resources/app/scripts/directives/header/header.js
+++ sentinel-dashboard-apollo/src/main/webapp/resources/app/scripts/directives/header/header.js
@@ -5,43 +5,49 @@
  * # adminPosHeader
  */
 angular.module('sentinelDashboardApp')
-    .directive('header', ['AuthService', function () {
-      return {
-        templateUrl: 'app/scripts/directives/header/header.html',
-        restrict: 'E',
-        replace: true,
-        controller: function ($scope, $state, $window, AuthService) {
-          if (!$window.localStorage.getItem("session_sentinel_admin")) {
-            AuthService.check().success(function (data) {
-              if (data.code == 0) {
-                $window.localStorage.setItem('session_sentinel_admin', JSON.stringify(data.data));
-                handleLogout($scope, data.data.id)
-              } else {
-                $state.go('login');
-              }
-            });
-          } else {
-            handleLogout($scope, JSON.parse($window.localStorage.getItem("session_sentinel_admin")).id)
+  .directive('header', ['VersionService', 'AuthService', function () {
+    return {
+      templateUrl: 'app/scripts/directives/header/header.html',
+      restrict: 'E',
+      replace: true,
+      controller: function ($scope, $state, $window, VersionService, AuthService) {
+        VersionService.version().success(function (data) {
+          if (data.code == 0) {
+            $scope.dashboardVersion = data.data;
           }
+        });
 
-          function handleLogout($scope, id) {
-            if (id == 'FAKE_EMP_ID') {
-              $scope.showLogout = false;
+        if (!$window.localStorage.getItem("session_sentinel_admin")) {
+          AuthService.check().success(function (data) {
+            if (data.code == 0) {
+              $window.localStorage.setItem('session_sentinel_admin', JSON.stringify(data.data));
+              handleLogout($scope, data.data.id)
             } else {
-              $scope.showLogout = true;
+              $state.go('login');
             }
-          }
+          });
+        } else {
+          handleLogout($scope, JSON.parse($window.localStorage.getItem("session_sentinel_admin")).id)
+        }
 
-          $scope.logout = function () {
-            AuthService.logout().success(function (data) {
-              if (data.code == 0) {
-                $window.localStorage.removeItem("session_sentinel_admin");
-                $state.go('login');
-              } else {
-                alert('logout error');
-              }
-            });
+        function handleLogout($scope, id) {
+          if (id == 'FAKE_EMP_ID') {
+            $scope.showLogout = false;
+          } else {
+            $scope.showLogout = true;
           }
         }
+
+        $scope.logout = function () {
+          AuthService.logout().success(function (data) {
+            if (data.code == 0) {
+              $window.localStorage.removeItem("session_sentinel_admin");
+              $state.go('login');
+            } else {
+              alert('logout error');
+            }
+          });
+        }
       }
-    }]);
+    }
+  }]);
